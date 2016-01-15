df0b7cfe609686d01bc8e1c1732e1e3f4cda9d81
Rassiel Rebustillo <rassiel.rebustillo@trintech.com>
2016-01-15 10:39:41 -0600

    create repo

---
+++ client/app/admin/admin.module.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('automaApp.admin', [
+  'automaApp.auth',
+  'ui.router'
+]);
---
+++ client/app/app.constant.js
@@ -0,0 +1,9 @@
+(function(angular, undefined) {
+'use strict';
+
+angular.module('automaApp.constants', [])
+
+.constant('appConfig', {userRoles:['guest','user','admin']})
+
+;
+})(angular);
